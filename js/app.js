4841528b59383db5ae8e43dcc3b54437aae67b1c
Maude Laperriere <1232645@cegeplimoilou.ca>
2017-01-26 14:37:40 -0500

    ajout_image_json

--- js/app.js
+++ js/app.js
@@ -129,7 +129,7 @@
                 map.removeLayer(iconesEtage);
                 map.removeLayer(numeroLocauxEtage);
 
-                if(etage == -1){
+                if(etage == "SS"){
 
                     // Le plan de base du sous-sol
                     imageUrl = 'images/etages/etage0_locaux.svg';
@@ -291,7 +291,8 @@
             var positionMarqueur = "";
 
             // Fonction appellé chaque fois que l'on veux afficher un marqueur sur la carte pour montrer l'emplacement d'un local
-            function afficherMarqueur(position, etage, message, image){
+            function afficherMarqueur(position, etage, message, image = null){
+                console.log (image);
 
                 // Suprime le calque "marqueur" qui contient le ou les marqueurs
                 map.removeLayer(marqueur);
@@ -302,9 +303,14 @@
                 // Affiche le bon étage selon le local
                 changerEtage(etage);
                 
-                // Crée un marqueur et le fait rebondir selon les paramètres "duration" et "height"
+
                 var button = '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#modalImage"><i class="material-icons">&#xE410;</i></button> <br><br>';
 
+                if (image == "null"){
+                    button = "";
+                }
+
+                // Crée un marqueur et le fait rebondir selon les paramètres "duration" et "height"
                 marqueur = L.marker(positionMarqueur, {bounceOnAdd: true, bounceOnAddOptions: {duration: 500, height: 200}}).bindPopup(button + message);
 
                 // Créé un nouveau calque, ajoute le marqueur puis l'affiche sur la carte
@@ -324,7 +330,7 @@
 
 
                // Modifie la source de l'image
-                $('#imgModal').attr("src","images/image_test.jpg");
+                $('#imgModal').attr("src","images/" + image);
 
                 //source pour titre message
                 $('#labelMessage').html(message);
@@ -509,7 +515,8 @@
                             numeroLocal,
                             positionLocal,
                             etageLocal,
-                            message;
+                            message,
+                            image = null;
                             /* console.info("Nombre de locaux à ajouter: " + data.length); */
                             for (i=0; i < data.length; i++)
                             {
@@ -518,7 +525,10 @@
                                 positionLocal = data[i].position;
                                 etageLocal = data[i].etage;
                                 message = data[i].message;
-
+                                image = null;
+                                if (data[i].hasOwnProperty("image")){
+                                    image=data[i].image;
+                                }
 
                                 // Selon chaque étage
                                 if (etageLocal == -1){
@@ -526,7 +536,7 @@
                                 }
 
                                 else if (etageLocal == 1){
-                                   elements += '<li><a href=\"javascript:afficherMarqueur(\'' + positionLocal  +'\'' + ',' + etageLocal + ',' + '\'<strong>' + nomLocal + '</strong><br>' + message + '\')">' + '<h3 class="nomLocal">' + nomLocal + '</h3><br><h6 class="etage">' + etageLocal +'er étage'+ '</h6><span class="codeLocalQ">Q' + numeroLocal + '</span><i class="material-icons pull-right">&#xE55E;</i></a></li>';
+                                   elements += '<li><a href=\"javascript:afficherMarqueur(\'' + positionLocal  +'\'' + ',' + etageLocal + ',' + '\'<strong>' + nomLocal + '</strong><br>' + message + '\',\'' + image + '\')">' + '<h3 class="nomLocal">' + nomLocal + '</h3><br><h6 class="etage">' + etageLocal +'er étage'+ '</h6><span class="codeLocalQ">Q' + numeroLocal + '</span><i class="material-icons pull-right">&#xE55E;</i></a></li>';
                                }
 
                                else{
